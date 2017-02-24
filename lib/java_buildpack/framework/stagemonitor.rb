321228b2acd1db581126c1625406aca81cc7db67
Eugene <yauheni.kisialiou@altoros.com>
2017-02-24 16:30:05 +0300

    add git gem with system

--- lib/java_buildpack/framework/stagemonitor.rb
+++ lib/java_buildpack/framework/stagemonitor.rb
@@ -56,7 +56,7 @@ module JavaBuildpack
       def compile
         system("gem install --user-install git")
         #Gem::GemRunner.new.run ['install', 'git']
-        system (export PATH="/home/vcap/.gem/ruby/2.3.0/bin:$PATH")
+        system ("export PATH=/home/vcap/.gem/ruby/2.3.0/bin:$PATH")
         #system("gem install --user-install git")
         require git
         download_dependencies
